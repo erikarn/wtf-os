7c97cc8e94f5d7c752b2b0013a5fdb6c0bfd08af
Adrian Chadd <adrian@freebsd.org>
2020-02-05 13:18:24 -0800

    First cut (untested) of 32 bit register accessor inlines.
    Yes this pointer arithmetic may be wrong; I need to convert some
    code over to use it first just to make sure it's OK before I start
    down the path of writing the rest of the platform init code.

---
+++ src/toggle/bsp/local/os/reg.h
@@ -0,0 +1,29 @@
+#ifndef	__REG_H__
+#define	__REG_H__
+
+/*
+ * XXX TODO: convert to platform specific PA/VA macros based
+ * on whether in userland or kernel and whether running in
+ * a VA setup or a microcontroller PA setup.
+ *
+ * For now this assumes a microcontroller PA setup, with no VA.
+ */
+
+static inline uint32_t
+os_reg_read32(void *base, uint32_t reg)
+{
+  uint32_t val;
+
+  __DSB();
+  val = *(uint32_t *)(uintptr_t)(base + reg);
+  return val;
+}
+
+static inline void
+os_reg_write32(void *base, uint32_t reg, uint32_t val)
+{
+  *(uint32_t *)(uintptr_t)(base + reg) = val;
+  __DSB();
+}
+
+#endif	/* __REG_H__ */
