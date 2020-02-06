829d6d35628b9161f63e4fda25be16a341eb928f
Adrian Chadd <adrian@freebsd.org>
2020-02-06 14:56:42 -0800

    Attempt to enable icache/dcache.  As of yet untested.

--- src/toggle/bsp/local/stm32f4/stm32f429_hw_flash.c
+++ src/toggle/bsp/local/stm32f4/stm32f429_hw_flash.c
@@ -8,11 +8,21 @@
 void
 stm32f429_hw_flash_enable_icache(void)
 {
+	uint32_t reg;
+
+	reg = os_reg_read32(FLASH_R_BASE, FLASH_ACR);
+	reg |= FLASH_ACR_ICEN;
+	os_reg_write32(FLASH_R_BASE, FLASH_ACR, reg);
 }
 
 void
 stm32f429_hw_flash_enable_dcache(void)
 {
+	uint32_t reg;
+
+	reg = os_reg_read32(FLASH_R_BASE, FLASH_ACR);
+	reg |= FLASH_ACR_DCEN;
+	os_reg_write32(FLASH_R_BASE, FLASH_ACR, reg);
 }
 
 void
