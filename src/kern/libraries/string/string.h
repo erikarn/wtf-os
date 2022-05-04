16e3196561a618828ea2f6789d92ed4cfdc60588
Adrian Chadd <adrian@FreeBSD.org>
2022-05-03 22:11:53 -0700

    [string] add strncmp

--- src/kern/libraries/string/string.h
+++ src/kern/libraries/string/string.h
@@ -4,5 +4,6 @@
 extern	size_t kern_strlen(const char *str);
 extern	size_t kern_strnlen(const char *str, size_t maxlen);
 extern	size_t kern_strlcpy(char *dst, const char *src, size_t dstlen);
+extern	int kern_strncmp(const char *s1, const char *s2, size_t len);
 
 #endif	/* __LIB_STRING_STRING_H__ */
---
+++ src/kern/libraries/string/strncmp.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2022 Adrian Chadd <adrian@freebsd.org>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-Licence-Identifier: GPL-3.0-or-later
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <kern/libraries/string/string.h>
+
+int
+kern_strncmp(const char *s1, const char *s2, size_t len)
+{
+	unsigned char u1, u2;
+
+	while (len-- > 0) {
+		u1 = (unsigned char) *s1++;
+		u2 = (unsigned char) *s2++;
+		if (u1 != u2)
+			return (u1 - u2);
+		if (u1 == '\0')
+			return (0);
+	}
+	return (0);
+}
