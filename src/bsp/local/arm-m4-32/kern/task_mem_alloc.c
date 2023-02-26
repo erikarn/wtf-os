3f680615ff02edf561fd202589dd8403b1aac99b
Adrian Chadd <adrian@FreeBSD.org>
2023-02-25 16:26:19 -0800

    [user_mem] check for allocator errors (returning 0)
    * check for allocator errors, return false if we fail here
    * remove the comment about freeing memory, since the owner owns and
      setup the task mem, it's up to it to free it if there's an error.
    * improve comments to remind me of things again

--- src/bsp/local/arm-m4-32/kern/task_mem_alloc.c
+++ src/bsp/local/arm-m4-32/kern/task_mem_alloc.c
@@ -36,10 +36,13 @@
 #include "kern/user/user_exec.h"
 
 #include <kern/core/task_mem_alloc.h>
+#include <kern/core/logging.h>
 
 
 #include "core/platform.h"
 #include "core/lock.h"
+
+LOGGING_EXT(LOG_TASKMEM);
 
 /**
  * Allocate RAM for a new user task.
@@ -62,27 +65,35 @@
  *
  * Returns true if the memory was allocated, false if it couldn't be.
  *
- * XXX TODO: error check allocations!
- * XXX TODO: free memory if we return an error!
+ * Note: the caller must call kern_task_mem_cleanup() to free any
+ * memory that we've allocated before we hit an error.
+ *
  * XXX TODO: any of the MPU related stuff.
  */
 bool
 platform_user_task_mem_allocate(const struct user_exec_program_header *hdr,
     struct user_exec_program_addrs *addrs,
-    struct task_mem *mem,
+    struct task_mem *tm,
     bool require_mpu)
 {
-	struct task_mem tm;
 	paddr_t kern_stack;
 
 	kern_bzero(addrs, sizeof(*addrs));
 
-	/* Allocate RAM - not MPU aligned for now */
-
+	/*
+	 * Allocate kernel stack - doesn't require MPU alignment,
+	 * only CPU alignment.
+	 */
 	kern_stack = kern_physmem_alloc(PLATFORM_DEFAULT_KERN_STACK_SIZE,
 	    PLATFORM_DEFAULT_KERN_STACK_ALIGNMENT,
 	    KERN_PHYSMEM_ALLOC_FLAG_ZERO);
+	if (kern_stack == 0) {
+		KERN_LOG(LOG_TASKMEM, KERN_LOG_LEVEL_CRIT,
+		     "failed to allocate %s", "kernel stack");
+		goto error;
+	}
 
+	/* Commented out reminder what we're just assuming is XIP for now */
 #if 0
 	/* XIP */
 	addrs.text_addr = pak.payload_start + hdr.text_offset;
@@ -91,10 +102,34 @@ platform_user_task_mem_allocate(const struct user_exec_program_header *hdr,
 	addrs.rodata_addr = pak.payload_start + hdr.rodata_offset;
 #endif
 
+	/* Allocate RAM - not MPU aligned for now */
 	addrs->got_addr = kern_physmem_alloc(hdr->got_size, 8, KERN_PHYSMEM_ALLOC_FLAG_ZERO);
+	if (addrs->got_addr == 0) {
+		KERN_LOG(LOG_TASKMEM, KERN_LOG_LEVEL_CRIT,
+		     "failed to allocate %s", "user GOT");
+		goto error;
+	}
+
 	addrs->bss_addr = kern_physmem_alloc(hdr->bss_size, 8, KERN_PHYSMEM_ALLOC_FLAG_ZERO);
+	if (addrs->bss_addr == 0) {
+		KERN_LOG(LOG_TASKMEM, KERN_LOG_LEVEL_CRIT,
+		     "failed to allocate %s", "user bss");
+		goto error;
+	}
+
 	addrs->data_addr = kern_physmem_alloc(hdr->data_size, 8, KERN_PHYSMEM_ALLOC_FLAG_ZERO);
+	if (addrs->data_addr == 0) {
+		KERN_LOG(LOG_TASKMEM, KERN_LOG_LEVEL_CRIT,
+		     "failed to allocate %s", "user data");
+		goto error;
+	}
+
 	addrs->heap_addr = kern_physmem_alloc(hdr->heap_size, 8, KERN_PHYSMEM_ALLOC_FLAG_ZERO);
+	if (addrs->heap_addr == 0) {
+		KERN_LOG(LOG_TASKMEM, KERN_LOG_LEVEL_CRIT,
+		     "failed to allocate %s", "user heap");
+		goto error;
+	}
 
 	/*
 	 * note - this is a stack, so the aligment (min 8 bytes) is for stack
@@ -102,6 +137,11 @@ platform_user_task_mem_allocate(const struct user_exec_program_header *hdr,
 	 * a PLATFORM define too.
 	 */
 	addrs->stack_addr = kern_physmem_alloc(hdr->stack_size, 8, KERN_PHYSMEM_ALLOC_FLAG_ZERO);
+	if (addrs->stack_addr == 0) {
+		KERN_LOG(LOG_TASKMEM, KERN_LOG_LEVEL_CRIT,
+		     "failed to allocate %s", "user stack");
+		goto error;
+	}
 
 	/* XIP */
 	/*
@@ -110,18 +150,20 @@ platform_user_task_mem_allocate(const struct user_exec_program_header *hdr,
 	 * (And I should pass in the whole of flash for text if I'm XIP'ing
 	 * and want it to be represented in the MPU.)
 	 */
-	kern_task_mem_set(&tm, TASK_MEM_ID_TEXT, 0x08000000, 0x200000, false);
+	kern_task_mem_set(tm, TASK_MEM_ID_TEXT, 0x08000000, 0x200000, false);
 
-	kern_task_mem_set(&tm, TASK_MEM_ID_USER_GOT, addrs->got_addr, hdr->got_size, true);
-	kern_task_mem_set(&tm, TASK_MEM_ID_USER_BSS, addrs->bss_addr, hdr->bss_size, true);
-	kern_task_mem_set(&tm, TASK_MEM_ID_USER_DATA, addrs->data_addr, hdr->data_size, true);
+	kern_task_mem_set(tm, TASK_MEM_ID_USER_GOT, addrs->got_addr, hdr->got_size, true);
+	kern_task_mem_set(tm, TASK_MEM_ID_USER_BSS, addrs->bss_addr, hdr->bss_size, true);
+	kern_task_mem_set(tm, TASK_MEM_ID_USER_DATA, addrs->data_addr, hdr->data_size, true);
 
 	/* XIP */
-	kern_task_mem_set(&tm, TASK_MEM_ID_USER_RODATA, addrs->rodata_addr, hdr->rodata_size, false);
+	kern_task_mem_set(tm, TASK_MEM_ID_USER_RODATA, addrs->rodata_addr, hdr->rodata_size, false);
 
-	kern_task_mem_set(&tm, TASK_MEM_ID_USER_HEAP, addrs->heap_addr, hdr->heap_size, true);
-	kern_task_mem_set(&tm, TASK_MEM_ID_USER_STACK, addrs->stack_addr, hdr->stack_size, true);
-	kern_task_mem_set(&tm, TASK_MEM_ID_KERN_STACK, kern_stack, PLATFORM_DEFAULT_KERN_STACK_SIZE, true);
+	kern_task_mem_set(tm, TASK_MEM_ID_USER_HEAP, addrs->heap_addr, hdr->heap_size, true);
+	kern_task_mem_set(tm, TASK_MEM_ID_USER_STACK, addrs->stack_addr, hdr->stack_size, true);
+	kern_task_mem_set(tm, TASK_MEM_ID_KERN_STACK, kern_stack, PLATFORM_DEFAULT_KERN_STACK_SIZE, true);
 
 	return (true);
+error:
+	return (false);
 }
