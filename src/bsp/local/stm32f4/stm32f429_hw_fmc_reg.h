bacb93bcbdf455b029846c2acd2044de8fe6d693
Adrian Chadd <adrian@FreeBSD.org>
2023-06-07 12:00:35 -0700

    [fmc] untested register definitions!

--- src/bsp/local/stm32f4/stm32f429_hw_fmc_reg.h
+++ src/bsp/local/stm32f4/stm32f429_hw_fmc_reg.h
@@ -44,8 +44,28 @@
 /* command register */
 #define	FMC_REG_SDCMR			0x150
 
+#define		FMC_REG_SDCMR_MODE_M	0x00000007
+#define		FMC_REG_SDCMR_MODE_S	2
+#define		FMC_REG_SDCMR_CTB2	BIT_U32(3)
+#define		FMC_REG_SDCMR_CTB1	BIT_U32(4)
+#define			FMC_REG_SDCMR_MODE_NORMAL	0
+#define			FMC_REG_SDCMR_MODE_CMD_CLK_EN	1
+#define			FMC_REG_SDCMR_MODE_CMD_PALL	2
+#define			FMC_REG_SDCMR_MODE_CMD_AUTO_REFRESH	3
+#define			FMC_REG_SDCMR_MODE_CMD_LOAD	4
+#define			FMC_REG_SDCMR_MODE_CMD_SELF_REFRESH	5
+#define			FMC_REG_SDCMR_MODE_CMD_POWER_DOWN	6
+#define		FMC_REG_SDCMR_NRFS_M	0x000001c0
+#define		FMC_REG_SDCMR_NRFS_S	5
+#define		FMC_REG_SDCMR_MRD_M	0x003ffe00
+#define		FMC_REG_SDCMR_MRD_S	9
+
 /* refresh timer register */
 #define	FMC_REG_SDRTR			0x154
+#define		FMC_REG_SDRTR_CRE	BIT_U32(0)
+#define		FMC_REG_SDRTR_COUNT_M	0x00003ffe
+#define		FMC_REG_SDRTR_COUNT_S	1
+#define		FMC_REG_SDRTR_REIE	BIT_U32(14)
 
 /* status register */
 #define	FMC_REG_SDSR			0x158
