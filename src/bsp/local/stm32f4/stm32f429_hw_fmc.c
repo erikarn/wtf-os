1308531178eacee1bd5e3cdfabc8adc45273d4c9
Adrian Chadd <adrian@FreeBSD.org>
2023-06-07 10:12:30 -0700

    [fmc] fix the command timeout check, add a comment
    * oops, command timeout check was wrong, so fix that
    * the command DWORD is actually a set of flags too, so I need to update
      things appropriately to make it generic enough.

--- src/bsp/local/stm32f4/stm32f429_hw_fmc.c
+++ src/bsp/local/stm32f4/stm32f429_hw_fmc.c
@@ -6,6 +6,16 @@
 #include "stm32f429_hw_fmc_reg.h"
 #include "stm32f429_hw_fmc.h"
 
+/* XXX */
+#include <stddef.h>
+#include "kern/console/console.h"
+
+/*
+ * XXX TODO: these commands have flag bits too so break things
+ * apart so we know things like which bank they're sent to.
+ */
+
+
 bool
 stm32f429_hw_fmc_send_command(uint32_t command)
 {
@@ -14,7 +24,7 @@ stm32f429_hw_fmc_send_command(uint32_t command)
 	os_reg_write32(FMC_R_BASE, FMC_REG_SDCMR, command);
 	timeout = 0xffff;
 	while ((timeout != 0) &&
-	     (os_reg_read32(FMC_R_BASE, FMC_REG_SDSR & 0x00000020) != 0)) {
+	     (os_reg_read32(FMC_R_BASE, FMC_REG_SDSR) & 0x00000020 != 0)) {
 		timeout--;
 	}
 
